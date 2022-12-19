ff2d0c67d0649948673260dd8a2092426339a51a
Aunesty <110820056+Aunestly@users.noreply.github.com>
2022-12-18 23:20:31 -0500

    Update script.js

--- Module4_solution/html/js/script.js
+++ Module4_solution/html/js/script.js
@@ -1,20 +1,59 @@
-var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[0]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
+//1
 
-for  (var i= 0; i < names.length; i++) {
-if(names[i].charAt(0) === 'J' || names[i].charAt(0)==='j'){
-console.log("Goodbye "+ names[i])
-}
-else{
-    console.log("Hello "+ names[i])
-}
-}
+// The idea of this assignment is to take an existing array of names
+// and then output either Hello 'Name' or Good Bye 'Name' to the console.
+// The program should say "Hello" to any name except names that start with a "J"
+// or "j", otherwise, the program should say "Good Bye". So, the final output
+// on the console should look like this:
+/*
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim*/
+
+/*Hello speaker function*/
+const helloSpeaker = {
+	speakWord: 'Hello',	
+};
+//This says Hello
+(function () {
+	helloSpeaker.speak = function (userName) {
+		return (helloSpeaker.speakWord + " " + userName);
+	}
+}());
+
+
+//2
+/*Goodbye speaker function*/
+const byeSpeaker = {
+	speakWord: 'Good Bye'
+};
+
+(function () {
+	byeSpeaker.speak = function (userName) {
+		return (byeSpeaker.speakWord + " " + userName);
+	}
+}());
+
+//3
+//This choose the names
+(function () {
+	var names = ['Yaakov', 'John', 'Jen', 'Jason', 'Paul', 'Frank', 'Larry', 'Paula', 'Laura', 'Jim'];
+	for (var i = 0; i < names.length; i++) {
+		var name = names[i];
+		var firstLetter = name.charAt(0).toLowerCase();
+		if (firstLetter === 'j') {
+			console.log(byeSpeaker.speak(name));
+			document.write(byeSpeaker.speak(name) + '<br>');
+		} else {
+			console.log(helloSpeaker.speak(name));
+			document.write(helloSpeaker.speak(name + '<br>'));
+		}
+	}
+})();
